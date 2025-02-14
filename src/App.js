d4bac530904952ab7f2cd2b31c02388973485f41
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2025-02-15 01:27:02 +0900

    feat: add initial content/styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Not-so Gumroad</h1>
+        <h3>Not-so neo-brutalism</h3>
       </header>
+
+      <main>
+        <section className="about">
+          <h2>About this website</h2>
+          <p>I'm just practising React. Did I spell practising right?</p>
+        </section>
+        <section className="projects">
+          <h2>Projects</h2>
+          <div className="project-grid">
+            <div className="project-card">
+              <h3>UFV Course Outline Finder</h3>
+              <p>a Chrome extension</p>
+            </div>
+            <div className="project-card">
+              <h3>japanhostelreviews.com</h3>
+              <p>a hostel review site</p>
+            </div>
+          </div>
+        </section>
+        <section className="contact">
+          <h2>Contact</h2>
+          <p>Email: notsogumroad@notsogumroad.com</p>
+          <p>GitHub: github.com/lyuhiroyama</p>
+        </section>
+      </main>
+
+      <footer>
+        <p>Last updated: 2025</p>
+      </footer>
     </div>
   );
 }
